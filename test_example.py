e29cd85b2a1d0c9970c9f49d4691512ecba694be
ashish <ashish.tp86@gmail.com>
2024-06-18 10:23:09 -0500

    test github ac

--- test_example.py
+++ test_example.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
-    print('asdas')
+    print('asdasdas')
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
