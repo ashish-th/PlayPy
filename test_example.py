ca1259d779262a85219fbc606d8d1376c98563ef
ashish <ashish.tp86@gmail.com>
2024-06-18 11:28:58 -0500

    push br2

--- test_example.py
+++ test_example.py
@@ -1,10 +1,12 @@
 import re
+
 from playwright.sync_api import Page, expect
 
 
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
     print('asdasdas')
+    assert False, "Failing test"
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
